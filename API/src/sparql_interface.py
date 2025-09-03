2b47a37e6f7652ff9def3df07dd112b296493abe
alessioromita <romitaalessio@gmail.com>
2025-09-03 14:26:30 +0200

    Change sparql interface log

--- API/src/sparql_interface.py
+++ API/src/sparql_interface.py
@@ -2,7 +2,7 @@ import logging
 
 import pandas as pd
 from SPARQLWrapper import SPARQLWrapper, JSON
-
+import traceback
 
 class SparqlDataFetcher:
     """
@@ -44,19 +44,25 @@ def _get_data_sparql(query: str, endpoint: str = None,
         logging.info(f"Query after formatting: {query}")
     else:
         logging.info("No params provided, using query as is")
+        logging.info(f"Query: {query}")
     base_query = query
-    sparql = SPARQLWrapper(endpoint)
-    sparql.setQuery(base_query)
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
-    result_processed = pd.json_normalize(results['results']['bindings'])
+    try:
+        sparql = SPARQLWrapper(endpoint)
+        sparql.setQuery(base_query)
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        result_processed = pd.json_normalize(results['results']['bindings'])
 
-    full_df = result_processed
-    # Clean .value columns
-    value_cols = [col for col in full_df.columns if col.endswith('.value')]
-    clean_df = full_df[value_cols]
+        full_df = result_processed
+        # Clean .value columns
+        value_cols = [col for col in full_df.columns if col.endswith('.value')]
+        clean_df = full_df[value_cols]
 
-    # Rename them to remove the `.value` suffix
-    clean_df.columns = [col.replace('.value', '') for col in clean_df.columns]
-    return clean_df
+        # Rename them to remove the `.value` suffix
+        clean_df.columns = [col.replace('.value', '') for col in clean_df.columns]
+        return clean_df
+    except Exception as e:
+        logging.error(f"Error executing SPARQL query: {e}")
+        logging.error(f"Traceback: {traceback.format_exc()}")
+        raise
 
